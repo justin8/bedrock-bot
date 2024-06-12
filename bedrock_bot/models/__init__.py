c2d4e0b28a6d914df85e70033c5498ec839b9c57
Justin Dray <justin@dray.be>
2024-06-12 14:42:10 -0700

    Move ConversationRole to the exports of models

--- bedrock_bot/models/__init__.py
+++ bedrock_bot/models/__init__.py
@@ -1,4 +1,5 @@
+from .base_model import ConversationRole
 from .claude import Claude3Haiku, Claude3Sonnet
 from .mistral import MistralLarge
 
-__all__ = ["Claude3Haiku", "Claude3Sonnet", "MistralLarge"]
+__all__ = ["Claude3Haiku", "Claude3Sonnet", "MistralLarge", "ConversationRole"]
